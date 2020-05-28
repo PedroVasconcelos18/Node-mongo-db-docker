b168663933ca7bfc355368666b9090f45f239b30
Pedro Henrique Vasconcelos <48729314+PedroVasconcelos18@users.noreply.github.com>
2020-05-28 20:46:55 -0300

    Add files via upload

---
+++ node-mongo-compose/backend/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// Database
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://db/mydb');
+
+//Middlewares
+server.use(bodyParser.urlencoded({extended: true}))
+server.use(bodyParser.json());
+server.use(cors());
+
+//mapemaneto objeto documento
+//ODM
+
+const Client = mongoose.model('Client', {
+    name: { type: String, required: true }
+})
+
+// Rest API
+Client.methods(['get', 'post', 'put', 'delete']);
+
+// Start Server
+server.listen(3000)
